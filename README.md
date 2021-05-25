7aa16fa4a6bae2d4dab2c7fc0ba7e3a076ebf013
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-05-25 12:45:36 -0700

    refactored and added README

---
+++ README.md
@@ -0,0 +1,45 @@
+# Funky Snake Game!
+
+Classic Snake game written on basic HTML, CSS, and JavaScript
+
+Play it [here](https://fritzchips.github.io/Snake-Game/)
+
+![Snake](./images/game.png)
+
+## Instructions
+
+UP ARROW = Snake Up
+
+LEFT ARROW = Snake Left
+
+RIGHT ARROW = Snake Right
+
+DOWN ARROW = Snake Down
+
+SPACEBAR = Pause Game
+
+Eat Apple:
+
+- +1 Score
+- +4s Time
+- Snake Size increase
+
+## Be Careful!
+
+- Time is running DOWN!
+- Longer Snake = Less time increase
+- Watch out for your surroundings!
+
+## Summary
+
+---
+
+I love to create games that will uplift and bring smiles to people. Snake is a simple but fun game to create. This was a great learning experience.
+
+Want a smile? Check out how the snake dies...
+
+## Author
+
+---
+
+- **Fritz Solibio** - _Junior Full-Stack Develepor In Training_
--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const c = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-const dimension = 20;
+const itemSize = 25;
 class Item {
   constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.dimension = dimension;
+    this.itemSize = itemSize;
     this.color = color;
   }
 
   draw() {
     c.fillStyle = this.color;
-    c.fillRect(this.x, this.y, this.dimension, this.dimension);
+    c.fillRect(this.x, this.y, this.itemSize, this.itemSize);
   }
 
   update() {
@@ -23,14 +23,25 @@ class Item {
 }
 const item = new Item();
 
-let timeTic = setInterval(countDown, 1000);
-const appendTimeValue = (seconds) => (seconds < 10 ? `0${seconds}` : seconds);
-let seconds = 15;
-function countDown() {
-  seconds--;
-  if (seconds === 0)
-    (gameState = "gameover"),
-      (causeOfDeath = "sSSsoooo tired...floating apples..?");
+const appleSpawnZones = [];
+for (
+  let cooridnates = itemSize;
+  cooridnates <= canvas.width - itemSize * 2;
+  cooridnates += itemSize
+) {
+  appleSpawnZones.push(cooridnates);
+}
+
+const snakeBody = [];
+const snakeColor = "#5975FB";
+for (let babySnake = 0; babySnake < 5; babySnake++) {
+  snakeBody.push(new Item(300 - babySnake * itemSize, 300, snakeColor));
+}
+
+const fruit = [];
+const appleColor = "#F00E33";
+for (let firstApple = 0; firstApple < 1; firstApple++) {
+  fruit.push(new Item(400, 300, appleColor));
 }
 
 let snakeMove = setInterval(snakeMovement, 100);
@@ -40,58 +51,32 @@ function snakeMovement() {
   switch (direction) {
     case "left":
       snakeBody.unshift(
-        new Item(snakeBody[0].x - dimension, snakeBody[0].y, snakeColor)
+        new Item(snakeBody[0].x - itemSize, snakeBody[0].y, snakeColor)
       );
-      snakeSpawnX = snakeBody[0].x - dimension;
-      snakeSpawnY = snakeBody[0].y;
       break;
     case "right":
       snakeBody.unshift(
-        new Item(snakeBody[0].x + dimension, snakeBody[0].y, snakeColor)
+        new Item(snakeBody[0].x + itemSize, snakeBody[0].y, snakeColor)
       );
-      snakeSpawnX = snakeBody[0].x + dimension;
-      snakeSpawnY = snakeBody[0].y;
       break;
     case "up":
       snakeBody.unshift(
-        new Item(snakeBody[0].x, snakeBody[0].y - dimension, snakeColor)
+        new Item(snakeBody[0].x, snakeBody[0].y - itemSize, snakeColor)
       );
-      snakeSpawnX = snakeBody[0].x;
-      snakeSpawnY = snakeBody[0].y - dimension;
       break;
     case "down":
       snakeBody.unshift(
-        new Item(snakeBody[0].x, snakeBody[0].y + dimension, snakeColor)
+        new Item(snakeBody[0].x, snakeBody[0].y + itemSize, snakeColor)
       );
-      snakeSpawnX = snakeBody[0].x;
-      snakeSpawnY = snakeBody[0].y + dimension;
+
       break;
   }
+  snakeSpawnX = snakeBody[snakeBody.length - 1].x;
+  snakeSpawnY = snakeBody[snakeBody.length - 1].y;
   snakeBody.pop();
 }
 
-const snakeBody = [];
-const snakeColor = "#9466FA";
-for (let babySnake = 0; babySnake < 5; babySnake++) {
-  snakeBody.push(new Item(300 - babySnake * dimension, 300, snakeColor));
-}
-
-const fruit = [];
-const appleColor = "#F00E33";
-for (let firstApple = 0; firstApple < 1; firstApple++) {
-  fruit.push(new Item(400, 300, appleColor));
-}
-
-const appleSpawnZones = [];
-for (
-  let cooridnates = 20;
-  cooridnates <= canvas.width - dimension * 2;
-  cooridnates += 20
-) {
-  appleSpawnZones.push(cooridnates);
-}
-
-onkeydown = changeDirection;
+document.onkeydown = changeDirection;
 function changeDirection(e) {
   switch (e.keyCode) {
     case 37:
@@ -161,7 +146,7 @@ function gameStateMaster() {
       clearInterval(timeTic);
       document.querySelector("#modal").style.display = "block";
       document.querySelector("#death").innerText = causeOfDeath;
-      let snakeSize = snakeBody.length * dimension;
+      let snakeSize = snakeBody.length * itemSize;
       document.querySelector("#stats").innerText = `Apples Eaten: ${score} 
             Snake Size: ${snakeSize}m `;
       break;
@@ -170,25 +155,25 @@ function gameStateMaster() {
 
 let score = 0;
 let causeOfDeath;
-function collisionTest() {
+function collisionTest(snakeHeadX, snakeHeadY) {
   if (
-    snakeBody[0].x + dimension > canvas.width ||
-    snakeBody[0].x < 0 ||
-    snakeBody[0].y + dimension > canvas.height ||
-    snakeBody[0].y < 0
+    snakeHeadX + itemSize > canvas.width ||
+    snakeHeadX < 0 ||
+    snakeHeadY + itemSize > canvas.height ||
+    snakeHeadY < 0
   ) {
     gameState = "gameover";
     causeOfDeath = "Ouchh.... Ittsss NOT an Apple Tree!?";
   }
 
   if (
-    snakeBody[0].x + dimension > fruit[0].x &&
-    snakeBody[0].x < fruit[0].x + dimension &&
-    snakeBody[0].y < fruit[0].y + dimension &&
-    snakeBody[0].y + dimension > fruit[0].y
+    snakeHeadX + itemSize > fruit[0].x &&
+    snakeHeadX < fruit[0].x + itemSize &&
+    snakeHeadY < fruit[0].y + itemSize &&
+    snakeHeadY + itemSize > fruit[0].y
   ) {
     score++;
-    seconds += 3;
+    score > 20 ? (seconds += 2) : score > 10 ? (seconds += 3) : (seconds += 4);
     if (seconds >= 20) seconds = 20;
 
     snakeBody.push(new Item(snakeSpawnX, snakeSpawnY, snakeColor));
@@ -198,38 +183,42 @@ function collisionTest() {
   }
 
   for (let hit = 1; hit < snakeBody.length; hit++) {
-    if (
-      snakeBody[0].x === snakeBody[hit].x &&
-      snakeBody[0].y === snakeBody[hit].y
-    ) {
+    if (snakeHeadX === snakeBody[hit].x && snakeHeadY === snakeBody[hit].y) {
       gameState = "gameover";
       causeOfDeath = "Feeling dissszzy iss..that mE...";
     }
   }
 }
 
+let timeTic = setInterval(countDown, 1000);
+const appendTimeValue = (seconds) => (seconds < 10 ? `0${seconds}` : seconds);
+let seconds = 15;
+function countDown() {
+  seconds--;
+  if (seconds === 0) {
+    gameState = "gameover";
+    causeOfDeath = "sSSsoooo tired...floating apples..?";
+  }
+}
+
 function animate() {
   requestAnimationFrame(animate);
+
   c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillStyle = "#66FAE2";
+  c.fillStyle = "#1EF060";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  for (let i = 0; i < snakeBody.length; i++) {
-    snakeBody[i].update();
-  }
-  for (let j = 0; j < fruit.length; j++) {
-    fruit[j].update();
-  }
+  for (let i = 0; i < snakeBody.length; i++) snakeBody[i].update();
 
-  collisionTest();
+  for (let j = 0; j < fruit.length; j++) fruit[j].update();
 
-  c.font = "30px Ariel";
-  c.fillStyle = "black";
-  c.textAlign = "left";
+  collisionTest(snakeBody[0].x, snakeBody[0].y);
+
+  (c.font = "30px Ariel"), (c.fillStyle = "black"), (c.textAlign = "left");
   c.fillText(
     `Apples: ${score} Time: 0:${appendTimeValue(seconds)}s`,
-    dimension,
-    canvas.height - dimension
+    itemSize,
+    canvas.height - itemSize
   );
 
   gameStateMaster();
